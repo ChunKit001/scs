86c157dba67ccbdb812d08336088e8907dcd4778
ck <ck@ck.ck>
2024-06-25 14:16:36 +0800

    update jdk21

--- scs-domain/src/main/java/com/scs/domain/customer/CustomerType.java
+++ scs-domain/src/main/java/com/scs/domain/customer/CustomerType.java
@@ -1,11 +1,6 @@
 package com.scs.domain.customer;
 
-/**
- * CustomerType
- *
- * @author Frank Zhang
- * @date 2018-01-08 8:51 AM
- */
+
 public enum CustomerType {
     POTENTIAL,
     INTENTIONAL,
