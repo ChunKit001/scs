9f6894ca4360eb106f46fb9c5f81ce0666a0e053
YH000928 <YH000928@danghuan.com>
2025-01-10 14:08:54 +0800

    commit

--- scs-start/src/main/java/com/scs/start/config/ValidConfig.java
+++ scs-start/src/main/java/com/scs/start/config/ValidConfig.java
@@ -1,15 +1,34 @@
 package com.scs.start.config;
 
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.hibernate.validator.HibernateValidator;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.validation.beanvalidation.SpringConstraintValidatorFactory;
 
 @Component
 public class ValidConfig {
+//    @Bean
+//    public LocalValidatorFactoryBean getValidatorFactory() {
+//        LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+//        localValidatorFactoryBean.getValidationPropertyMap().put("hibernate.validator.fail_fast", "true");
+//        return localValidatorFactoryBean;
+//    }
+
     @Bean
-    public LocalValidatorFactoryBean getValidatorFactory() {
-        LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
-        localValidatorFactoryBean.getValidationPropertyMap().put("hibernate.validator.fail_fast", "true");
-        return localValidatorFactoryBean;
+    public Validator validator(AutowireCapableBeanFactory springFactory) {
+        try (ValidatorFactory factory = Validation.byProvider(HibernateValidator.class)
+                .configure()
+                // 快速失败
+                .failFast(true)
+                // 解决 SpringBoot 依赖注入问题
+                .constraintValidatorFactory(new SpringConstraintValidatorFactory(springFactory))
+                .buildValidatorFactory()) {
+            return factory.getValidator();
+        }
     }
 }
